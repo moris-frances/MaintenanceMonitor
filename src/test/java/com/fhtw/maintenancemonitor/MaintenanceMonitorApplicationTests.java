3f50009915dd19a7ebf6fcb452e8f6d2c424ef41
Ahmad Schurbaji <schurbaji.ahmad@gmail.com>
2022-11-17 20:45:44 +0100

    added unit test for resetMessage Method

--- src/test/java/com/fhtw/maintenancemonitor/MaintenanceMonitorApplicationTests.java
+++ src/test/java/com/fhtw/maintenancemonitor/MaintenanceMonitorApplicationTests.java
@@ -2,7 +2,6 @@ package com.fhtw.maintenancemonitor;
 
 
 import com.fhtw.maintenancemonitor.service.MessageService;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
@@ -37,6 +37,15 @@ class MaintenanceMonitorApplicationTests {
 				.andExpect(status().isOk())
 				.andExpect(content().contentType(MediaType.valueOf("text/plain;charset=UTF-8")))
 				.andExpect(content().string("Everything works as expected"));
+	}
+
+	@Test
+	public void resetMessageTest() throws Exception {
+		RequestBuilder putRequest = MockMvcRequestBuilders.put("/api/message/reset");
+		mockMvc.perform(putRequest).andExpect(status().isOk());
+		String expected = "Everything works as expected";
+		String actual = messageService.getMessage();
+		assertEquals(expected, actual);
 	}
 
 }
