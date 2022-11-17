6f4493fb43d16d6a917129aaa47a782a73378624
Ahmad Schurbaji <schurbaji.ahmad@gmail.com>
2022-11-17 20:31:28 +0100

    added unit test for getMessage Method

--- src/test/java/com/fhtw/maintenancemonitor/MaintenanceMonitorApplicationTests.java
+++ src/test/java/com/fhtw/maintenancemonitor/MaintenanceMonitorApplicationTests.java
@@ -1,13 +1,42 @@
 package com.fhtw.maintenancemonitor;
 
+
+import com.fhtw.maintenancemonitor.service.MessageService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class MaintenanceMonitorApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+
+	@Autowired
+	MessageService messageService;
+
+	@BeforeEach
+	void clear(){
+		messageService.resetMessage();
+	}
 	@Test
-	void contextLoads() {
+	public void getMessageTest() throws Exception {
+		RequestBuilder getRequest = MockMvcRequestBuilders.get("/api/message");
+		mockMvc.perform(getRequest)
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.valueOf("text/plain;charset=UTF-8")))
+				.andExpect(content().string("Everything works as expected"));
 	}
 
 }
