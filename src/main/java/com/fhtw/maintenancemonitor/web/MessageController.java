f7ccc8d576b253549285e231888b9be316b2e9ee
Ahmad Schurbaji <schurbaji.ahmad@gmail.com>
2022-11-17 20:06:03 +0100

    create Rest method getMessage

---
+++ src/main/java/com/fhtw/maintenancemonitor/web/MessageController.java
@@ -0,0 +1,27 @@
+package com.fhtw.maintenancemonitor.web;
+
+
+import com.fhtw.maintenancemonitor.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MessageController {
+
+    private final MessageService messageService;
+
+    @Autowired
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @GetMapping(value = "/api/message")
+    public ResponseEntity<String> getMessage() {
+        return ResponseEntity.ok(messageService.getMessage());
+    }
+
+
+
+
+}
