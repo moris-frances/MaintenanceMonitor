529d866ffd4aefe499597cb10b29d2b01d9a8e1c
Ahmad Schurbaji <schurbaji.ahmad@gmail.com>
2022-11-17 19:59:46 +0100

    add default getMessage methode

---
+++ src/main/java/com/fhtw/maintenancemonitor/service/MessageService.java
@@ -0,0 +1,18 @@
+package com.fhtw.maintenancemonitor.service;
+
+import com.fhtw.maintenancemonitor.exception.MessageException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageService{
+    private String message;
+
+    public String getMessage(){
+        String defaultMessage = "Everything works as expected";
+        return  (message != null && !message.isEmpty()) ? message : defaultMessage;
+    }
+
+
+
+
+}
